eb8c232b909900c2612d99e7a2a2c624b7543b1a
bojidaryovchev <blizzardcon10@gmail.com>
2022-02-13 00:23:29 +0200

    introduced the Providers component to wrap up all of our providers/contexts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app';
 import Layout from 'src/components/organisms/Layout';
+import Providers from 'src/components/organisms/Providers';
 import 'styles/index.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Providers>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </Providers>
   );
 }
 
