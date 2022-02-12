1bacd3b9702897795d0c38adbbfbe65ddfd67e86
bojidaryovchev <blizzardcon10@gmail.com>
2022-02-12 22:05:01 +0200

    initialized TailwindCSS following this guide: https://tailwindcss.com/docs/guides/nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import 'styles/index.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default App;
