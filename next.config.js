c7b11a82161daf1cc0a2f1783556c4dceee582fd
bojidaryovchev <blizzardcon10@gmail.com>
2022-02-26 00:52:30 +0200

    next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: 'experimental-serverless-trace',
+  experimental: {
+    outputStandalone: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
