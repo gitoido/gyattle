f8a88a740ee8c6037e7271def4a107729bd10c88
bojidaryovchev <blizzardcon10@gmail.com>
2022-02-26 00:59:59 +0200

    reverted to next v11 as api routes seem to not work in v12

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
